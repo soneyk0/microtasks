8446d63c6a148255184ff9697268b68ea93e4fb4
Yana <yana.prokopenko24@mai.ru>
2023-05-26 21:10:24 +0300

    button part 2 universal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 
 function App() {
+
+    const Button1Foo = (subscriber:string, age:number, adres: string) => {
+        console.log(subscriber, age, adres)
+    }
+    const Button2Foo = (subscriber:string, age:number) => {
+        console.log(subscriber, age)
+    }
+const Button3Foo =()=>{
+        console.log('i stuped button')
+}
     return (
         <div className={'App'}>
-            <button>MyTouTubeChanel-1</button>
-            <button>MyTouTubeChanel-1</button>
+            <Button name={'MyYouTubeChanel-1'} callBack={()=>Button1Foo('im Yana', 21, 'live in Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={()=>Button2Foo('im Vasya', 18)}/>
+            <Button name={'Stuped'} callBack={Button3Foo}/>
         </div>
+
     );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type ButtonProps = {
+    name: string
+    callBack: () => void
+
+}
+export const Button = (props: ButtonProps) => {
+
+    const onClickHandler = () => {
+        props.callBack()
+
+    }
+    return (
+        <button onClick={onClickHandler}>{props.name}</button>
+    )
+}
